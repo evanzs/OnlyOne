54a54ac33e96cb172dbad65d823b354cca40f068
evanzs <evandrofidk@hotmail.com>
2022-12-15 20:52:46 -0300

    feat(mockWords)::sparkles: create words mock

---
+++ src/app/server/mockWords.ts
@@ -0,0 +1,9 @@
+import { Words } from "../Interfaces/words";
+
+export const WORDS: Words[] = [
+    { id: 1, name: 'TESTE1' }, 
+    { id: 2, name: 'TESTE2' }, 
+    { id: 3, name: 'TESTE3' }, 
+    { id: 4, name: 'TESTE4' }, 
+    { id: 5, name: 'TESTE5' }
+];

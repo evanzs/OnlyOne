266beb7dd7078c080c4d66ef105394cf4818c316
evanzs <evandrofidk@hotmail.com>
2022-12-24 12:21:30 -0300

    refactor(app.module)::hammer: add new component WordDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeverComponent } from './server/server.component';
+import { WordDetailComponent } from './word-detail/word-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SeverComponent
+    SeverComponent,
+    WordDetailComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
